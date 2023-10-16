c8173a991a967214c976119384587f80e41ce52c
Furover <fur.wiggywag@protonmail.com>
2023-10-16 18:29:17 -0300

    still sheppin

--- src/components/foodlist/index.js
+++ src/components/foodlist/index.js
@@ -1,5 +1,7 @@
 import { View, StyleSheet, Text, TouchableOpacity, Image } from 'react-native'
 
+import { LinearGradient } from 'expo-linear-gradient'
+
 export function FoodList({ data }){
     return(
         <TouchableOpacity activeOpacity={0.85} style={styles.container}>
@@ -9,8 +11,12 @@ export function FoodList({ data }){
             />
             <View style={styles.info}>
                 <Text style={styles.name}>{data.name}</Text>
-                <Text style={styles.desc}>{data.total_ingredients} | {data.time} min</Text>
+                <Text style={styles.desc}>{data.total_ingredients} Ingredientes | {data.time} min</Text>
             </View>
+            <LinearGradient
+                style={styles.gradient}
+                colors={['transparent', 'rgba(0,0,0,0.70)', 'rgba(0,0,0, 0.95)']}
+            />
         </TouchableOpacity>
     )
 }
@@ -23,5 +29,28 @@ const styles = StyleSheet.create({
         width: '100%',
         height: 200,
         borderRadius: 14
+    },
+    info:{
+        position: 'absolute',
+        bottom: 14,
+        left: 14,
+        zIndex: 99
+    },
+    name:{
+        fontSize: 18,
+        color: "#FFF",
+        fontWeight: 'bold'
+    },
+    desc:{
+        color: "#FFF"
+    },
+    gradient:{
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        bottom: 0,
+        height: '55%',
+        borderRadius: 14,
+        zIndex: 1
     }
 })
